34bd87c1cea2803a01e3ff2714819011478eb4d0
debenedettohernan <hernan.debenedetto@hotmail.com>
2025-01-09 23:52:55 -0300

    primer componente

---
+++ src/pages/customers/CustomerList.tsx
@@ -0,0 +1,69 @@
+import { IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from 'react-router';
+import ExploreContainer from '../../components/ExploreContainer';
+
+const CustomerList: React.FC = () => {
+
+  const { name } = useParams<{ name: string; }>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>{name}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        
+      <IonGrid>
+        <IonRow style={{ backgroundColor: '#DDD' }}>
+          <IonCol>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+          <IonCol>4</IonCol>
+          <IonCol>5</IonCol>
+          <IonCol>6</IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+          <IonCol>4</IonCol>
+          <IonCol>5</IonCol>
+          <IonCol>6</IonCol>
+          <IonCol>7</IonCol>
+          <IonCol>8</IonCol>
+          <IonCol>9</IonCol>
+          <IonCol>10</IonCol>
+          <IonCol>11</IonCol>
+          <IonCol>12</IonCol>
+        </IonRow>
+      </IonGrid>
+        
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default CustomerList;
