b7ec04ef18c5289a641f4dad43b79c2da78971ef
debenedettohernan <hernan.debenedetto@hotmail.com>
2025-01-11 13:43:16 -0300

    armando tabla de clientes

--- src/pages/customers/CustomerList.tsx
+++ src/pages/customers/CustomerList.tsx
@@ -1,6 +1,7 @@
-import { IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../../components/ExploreContainer';
+import { add, pencil } from 'ionicons/icons';
 const CustomerList: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
@@ -12,53 +13,56 @@ const CustomerList: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>{name}</IonTitle>
+          <IonTitle>Gestion de clientes</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonGrid>
-          <IonRow>
-            <IonCol>1</IonCol>
-            <IonCol>2</IonCol>
-            <IonCol>3</IonCol>
-          </IonRow>
-        </IonGrid>
+        <IonCard>
+          <IonButton fill='solid' slot='end' size='default'> <IonIcon icon={add} >
+          </IonIcon>Agregar Cliente
+          </IonButton>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">{name}</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonGrid>
+            <IonRow>
+              <IonCol>Nombre</IonCol>
+              <IonCol>Email</IonCol>
+              <IonCol>Telefono</IonCol>
+              <IonCol>Direccion</IonCol>
+              <IonCol>Acciones</IonCol>
+            </IonRow>
+          </IonGrid>
 
-        <IonGrid>
-          <IonRow>
-            <IonCol>1</IonCol>
-            <IonCol>2</IonCol>
-            <IonCol>3</IonCol>
-            <IonCol>4</IonCol>
-            <IonCol>5</IonCol>
-            <IonCol>6</IonCol>
-          </IonRow>
-        </IonGrid>
-
-        <IonGrid>
-          <IonRow>
-            <IonCol>1</IonCol>
-            <IonCol>2</IonCol>
-            <IonCol>3</IonCol>
-            <IonCol>4</IonCol>
-            <IonCol>5</IonCol>
-            <IonCol>6</IonCol>
-            <IonCol>7</IonCol>
-            <IonCol>8</IonCol>
-            <IonCol>9</IonCol>
-            <IonCol>10</IonCol>
-            <IonCol>11</IonCol>
-            <IonCol>12</IonCol>
-          </IonRow>
-        </IonGrid>
-
-
+          <IonGrid>
+            <IonRow>
+              <IonCol>Hernan Debenedetto</IonCol>
+              <IonCol>hernandebe@hotmail.com</IonCol>
+              <IonCol>9999999</IonCol>
+              <IonCol>sixto laspiur</IonCol>
+              <IonCol>Acciones</IonCol>
+            </IonRow>
+            <IonButton>
+              <IonIcon icon={pencil} slot='icon-only'/>
+            </IonButton>
+          </IonGrid>
+          
+          <IonGrid>
+            <IonRow>
+              <IonCol>Hernan Debenedetto</IonCol>
+              <IonCol>hernandebe@hotmail.com</IonCol>
+              <IonCol>9999999</IonCol>
+              <IonCol>sixto laspiur</IonCol>
+              <IonCol>Acciones</IonCol>
+            </IonRow>
+            <IonButton>
+              <IonIcon icon={pencil} slot='icon-only'/>
+            </IonButton>
+          </IonGrid>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
